522750ac429c7739cf083de5329d5666b85773d0
Link-Fight <315148032@qq.com>
2018-11-27 10:09:29 +0800

    build

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,10 @@
 module.exports = {
   baseUrl: process.env.NODE_ENV === 'git'
     ? '/mall/dist/'
-    : '/',
+    : '',
   configureWebpack: {
   },
+  outputDir: process.env.NODE_ENV === 'production' ? 'C:/xampp/htdocs/agri_bpmn_v3/trunk/public/mall2' : 'dist',
   devServer: {
     proxy: 'http://localhost:97'
   }
